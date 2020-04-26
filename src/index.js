b69f7c82a4d0c1a0a4ce44abc80d92147673b0c2
Tyler Sargent <tsargent@gmail.com>
2020-04-26 11:08:29 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,58 +1,10 @@
-// from Jeff:
-// auth currently returns this info: 
-// {
-//   "scopes": [
-//       "scim:write",
-//       "scim:read"
-//   ],
-//   "expires_in_seconds": 43199,
-//   "application": {
-//       "uid": "5mRjqdD"
-//   },
-//   "created_at": 1585597311,
-//   "token": {
-//       "access_token": null
-//   }
-// }
-// when we bump the expiration it just adds the time to expires_in . 
-// So the actual time that the token expires is created_at  + expires_in in unix timestamp.
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Link, useNavigate } from "@reach/router"
+import { Router, Link } from "@reach/router"
 import App from './App';
-import AutoLogout2 from "./Autologout2";
+import AutoLogout from "./Autologout";
 import "bootstrap/dist/css/bootstrap.css";
 import 'promise-polyfill/src/polyfill';
-
-const nowSeconds = () => Math.floor(Date.now() / 1000);
-
-let createdAt;
-let expiresAt;
-
-const setExpiration = () => new Promise((resolve) => {
-  setTimeout(() => {
-    createdAt = nowSeconds();
-    const interval = 20;
-    expiresAt = createdAt + interval;
-    const payload = {
-      created_at: createdAt,
-      expires_at: expiresAt,
-      expires_in_seconds: interval,
-    }
-    localStorage.setItem('fakeAuth', JSON.stringify(payload));
-    resolve(payload);
-  }, 200);
-});
-
-const getExpiration = () => new Promise((resolve) => {
-  setTimeout(() => {
-    const authData = JSON.parse(localStorage.getItem('fakeAuth'));
-    // const { expires_at } = authData;
-    // const now = nowSeconds();
-    resolve(authData);
-  }, 200)
-})
 
 const Notifier = ({ onClickContinue }) => {
   return (
@@ -75,19 +27,10 @@ const Notifier = ({ onClickContinue }) => {
   )
 }
 
-const SignIn = () => (
-  <>
-    <h2>Sign in</h2>
-    <Link to="/" className="btn btn-primary">Sign in</Link>
-  </>
-)
-
 const Main = () => {
-  const navigate = useNavigate();
-  const logout = () => navigate("sign-in");
   return (
     <div>
-      <AutoLogout2 setExpiration={setExpiration} getExpiration={getExpiration} onTimeout={logout}>
+      <AutoLogout>
         {(args) => (
           <>
             <pre className="bg-dark text-white p-4 m-4">
@@ -98,7 +41,7 @@ const Main = () => {
             )}
           </>
         )}
-      </AutoLogout2>
+      </AutoLogout>
       <App />   
     </div>
   )
@@ -107,7 +50,6 @@ const Main = () => {
 ReactDOM.render(
   <Router>
     <Main path="/" />
-    <SignIn path="sign-in" />
   </Router>,
   document.getElementById('root')
 );
